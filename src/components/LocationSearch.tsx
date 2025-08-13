dc37aab5e8974ce06cc9634e6bcb20be6726d184
Daniela Toncheva <daniela.toncheva@scalefocus.com>
2025-08-13 13:55:28 +0300

    no locations string added

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -29,18 +29,20 @@ const [term, setTerm] = useState('');
         </form>
         <h1 className='font-bold mt-6'>Found Locations</h1>
         <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
-            {places?.map(place => {
-                return <Fragment key={place.id}>
-                    <p className="text-sm">{place.name}</p>
-                    <button
-                        className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
-                        onClick={() => onPlaceClick(place)}
-                    >Go
-                    </button>
-                    <div className='border-b w-full col-span-2' />
+            {places.length === 0 
+                ? places?.map(place => {
+                    return <Fragment key={place.id}>
+                        <p className="text-sm">{place.name}</p>
+                        <button
+                            className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                            onClick={() => onPlaceClick(place)}
+                        >Go
+                        </button>
+                        <div className='border-b w-full col-span-2' />
 
-                </Fragment>
-            })}
+                    </Fragment>
+                    })
+                : <p className='mt-2'>No locations found</p>}
         </div>
     </div>
   )
